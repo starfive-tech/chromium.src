0a91475303f2d4acca3a638afaaa5a6ea88c08fa
Rebecca Chang Swee Fun <rebecca.chang@starfivetech.com>
2022-09-07 21:27:21 -0400

    ignore unreachable-code-break
    Signed-off-by: Rebecca Chang Swee Fun <rebecca.chang@starfivetech.com>

--- chrome/browser/sessions/session_service.cc
+++ chrome/browser/sessions/session_service.cc
@@ -1,6 +1,7 @@
 // Copyright 2012 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+#pragma clang diagnostic ignored "-Wunreachable-code"
 
 #include "chrome/browser/sessions/session_service.h"
 
--- chrome/browser/ui/views/tab_icon_view.cc
+++ chrome/browser/ui/views/tab_icon_view.cc
@@ -1,6 +1,7 @@
 // Copyright 2012 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+#pragma clang diagnostic ignored "-Wunreachable-code"
 
 #include "chrome/browser/ui/views/tab_icon_view.h"
 #include "ui/gfx/scoped_canvas.h"
